9ff50a8d1532cf98f4efa54a67d6cf83d43d535c
Joey Ebach <connect@joeyebach.com>
2023-08-17 22:06:10 -0500

    modularized all of the code

---
+++ components/events/domEvents.js
@@ -0,0 +1,20 @@
+import { students } from '../../utils/data/studentsVoldy';
+import studentsOnDom from '../../utils/studentsOnDom';
+
+const domEvents = () => {
+  // target filter buttons on Dom
+  document.querySelector('#filter-container').addEventListener('click', (e) => {
+    if (e.target.id.includes('filter')) {
+      const [, house] = e.target.id.split('--');
+
+      if (house === 'all') {
+        studentsOnDom('#students', students);
+      } else if (house) {
+        const filter = students.filter((student) => student.house === house);
+        studentsOnDom('#students', filter, house);
+      }
+    }
+  });
+};
+
+export default domEvents;
